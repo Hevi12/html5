8c849ccf0f6a2ea947058ab8843566187f88d003
Hevi12 <37109889+Hevi12@users.noreply.github.com>
2018-03-06 23:47:34 +0700

    Add files via upload

---
+++ Proweb 1/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menu HTML</title>
+    </head>
+    <body>
+        <h1>WELCOM</h1>
+         <h3>Data Pribadi</h3>
+            <pre>
+                Nama :Hevi Siti Haryati
+                Nim  :5150411111
+                Kelas:Informatika B
+            </pre>
+          <p>Tugas Elerning Pemrograman WEB mengenai dasar-dasar HTML</p>
+        <h1>1.JADWAL PIKET KOS </h1>
+        <table style="border: 3px blue solid">
+            <tr style="text-align: 3px red solid">
+                <td style="border: 2px red solid">Hari</td>
+                <td style="border: 2px red solid">Nama</td>
+                <td style="border: 2px red solid">Tempat Piket</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Senin</td>
+                <td style="border: 2px black solid">Nana</td>
+                <td style="border: 2px black solid">Menyapu</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Selasa</td>
+                <td style="border: 2px black solid">Zizah</td>
+                <td style="border: 2px black solid">Mengepel</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Rabu</td>
+                <td style="border: 2px black solid">Evi</td>
+                <td style="border: 2px black solid">Mencuci Piring</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Kamis</td>
+                <td style="border: 2px black solid">Nisa</td>
+                <td style="border: 2px black solid">Menyapu</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Jumat</td>
+                <td style="border: 2px black solid">Desti</td>
+                <td style="border: 2px black solid">Mengepel</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Sabtu</td>
+                <td style="border: 2px black solid">Fatma</td>
+                <td style="border: 2px black solid">Mencuci piring</td>
+            </tr>
+    </table>
+             <h1>2.JADWAL KULIAH </h1>
+        <table style="border: 3px blue solid">
+            <tr style="text-align: 3px red solid">
+           <td style="border: 2px red solid">Hari</td>
+                <td style="border: 2px red solid">Jadwal</td>
+                <td style="border: 2px red solid">Ruang</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid" rowspan="2">Senin</td>
+                <td style="border: 2px black solid">Pemrograman WEB</td>
+                <td style="border: 2px black solid">Lab 7</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Kecerdasan Buatan</td>
+                <td style="border: 2px black solid">D14</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Selasa</td>
+                <td style="border: 2px black solid">Pemrograman Client Server</td>
+                <td style="border: 2px black solid">Lab 9</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Rabu</td>
+                <td style="border: 2px black solid">Pemrograman WEB Teori</td>
+                <td style="border: 2px black solid">F01</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid" rowspan="2">Kamis</td>
+                <td style="border: 2px black solid">Logika Fuzzy</td>
+                <td style="border: 2px black solid">H34</td>
+            </tr>
+            <tr>
+                <td style="border: 2px black solid">Pengenalan Pola</td>
+                <td style="border: 2px black solid">D22</td>
+            </tr>
+             <tr>
+                <td style="border: 2px black solid">Jumat</td>
+                <td style="border: 2px black solid">Pemrograman Client Server Teori</td>
+                <td style="border: 2px black solid">H34</td>
+            </tr>
+        </table>
+    </body>
+</html>
